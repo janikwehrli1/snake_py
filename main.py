117735a53be04a9cfbab5570e4c250d413d3b193
janikwibilea <janik.wehrli@bbz-sh.ch>
2021-02-25 16:23:17 +0100

    classes

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ screen.setup(width=600, height=600)
 screen.title("Snake")
 screen.tracer(0)
 screen.bgcolor("black")
+
 mysnake = Snake()
+
+screen.listen()
+screen.onkey(mysnake.up,"Up")
+screen.onkey(mysnake.down, "Down")
+screen.onkey(mysnake.left,"Left")
+screen.onkey(mysnake.right,"Right")
+
 
 game_not_end = True
 while game_not_end:
--- snake.py
+++ snake.py
@@ -1,12 +1,16 @@
 from turtle import Turtle
 
 starting_pos = [(0, 0), (-20, 0), (-40, 0)]
-
+up = 90
+right = 0
+down = 270
+left = 180
 
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for i in starting_pos:
@@ -22,4 +26,19 @@ class Snake:
             new_x = self.segments[i - 1].xcor()
             new_y = self.segments[i - 1].ycor()
             self.segments[i].goto(new_x, new_y)
-        self.segments[0].forward(20)
+        self.head.forward(20)
+
+    def up(self):
+        if self.head.heading() != down:
+            self.head.setheading(up)
+
+    def down(self):
+        if self.head.heading() != up:
+            self.head.setheading(down)
+
+    def right(self):
+        if self.head.heading() != left:
+            self.head.setheading(right)
+    def left(self):
+        if self.head.heading() != right:
+            self.head.setheading(left)
