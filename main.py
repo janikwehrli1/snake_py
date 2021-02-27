f8e53a35fcbc74e638fc1a313fa22911a3e5c279
janikwibilea <janik.wehrli@bbz-sh.ch>
2021-02-27 17:25:15 +0100

    collison

--- main.py
+++ main.py
@@ -16,11 +16,10 @@ myfood = Food()
 myscore = Scoreboard()
 
 screen.listen()
-screen.onkey(mysnake.up,"Up")
+screen.onkey(mysnake.up, "Up")
 screen.onkey(mysnake.down, "Down")
-screen.onkey(mysnake.left,"Left")
-screen.onkey(mysnake.right,"Right")
-
+screen.onkey(mysnake.left, "Left")
+screen.onkey(mysnake.right, "Right")
 
 game_not_end = True
 while game_not_end:
@@ -31,11 +30,19 @@ while game_not_end:
     if mysnake.head.distance(myfood) < 20:
         print("num")
         myfood.random_location()
+        mysnake.extend()
         myscore.score_increase()
         myscore.update_scoreboard()
 
     if mysnake.head.xcor() > 280 or mysnake.head.xcor() < -280 or mysnake.head.ycor() > 280 or mysnake.head.ycor() < -280:
         game_not_end = False
         myscore.game_over()
+
+    for i in mysnake.segments:
+        if i == mysnake.head:
+            pass
+        elif mysnake.head.distance(i) < 10:
+            game_not_end = False
+            myscore.game_over()
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -15,12 +15,18 @@ class Snake:
 
     def create_snake(self):
         for i in starting_pos:
-            new_segment = Turtle()
-            new_segment.penup()
-            new_segment.shape("square")
-            new_segment.color("red")
-            new_segment.goto(i)
-            self.segments.append(new_segment)
+            self.add_segment(i)
+
+    def add_segment(self, i):
+        new_segment = Turtle()
+        new_segment.penup()
+        new_segment.shape("square")
+        new_segment.color("red")
+        new_segment.goto(i)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for i in range(len(self.segments) - 1, 0, -1):
