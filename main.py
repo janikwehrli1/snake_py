41e50fe866fddd09fa72a0feb1bea5a53fdc6041
janikwibilea <janik.wehrli@bbz-sh.ch>
2021-02-27 17:10:42 +0100

    collison

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from turtle import Screen, Turtle
 import time
 from snake import Snake
 from snakefood import Food
+from scoreboard import Scoreboard
+import random
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -11,6 +13,7 @@ screen.bgcolor("green")
 
 mysnake = Snake()
 myfood = Food()
+myscore = Scoreboard()
 
 screen.listen()
 screen.onkey(mysnake.up,"Up")
@@ -25,6 +28,14 @@ while game_not_end:
     time.sleep(0.1)
     mysnake.move()
 
+    if mysnake.head.distance(myfood) < 20:
+        print("num")
+        myfood.random_location()
+        myscore.score_increase()
+        myscore.update_scoreboard()
 
+    if mysnake.head.xcor() > 280 or mysnake.head.xcor() < -280 or mysnake.head.ycor() > 280 or mysnake.head.ycor() < -280:
+        game_not_end = False
+        myscore.game_over()
 
 screen.exitonclick()
--- snakefood.py
+++ snakefood.py
@@ -8,9 +8,12 @@ class Food(Turtle):
         self.shape("circle")
         self.color("blue")
         self.penup()
-        self.shapesize(0.5, 0.5)
+        self.shapesize(1, 1)
         rand1 = random.randint(-280, 280)
         rand2 = random.randint(-280, 280)
         self.goto(rand1, rand2)
 
-
+    def random_location(self):
+        rand1 = random.randint(-280, 280)
+        rand2 = random.randint(-280, 280)
+        self.goto(rand1, rand2)
